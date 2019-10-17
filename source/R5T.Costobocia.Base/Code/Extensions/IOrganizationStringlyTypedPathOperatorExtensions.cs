e5667bbdbc595c10098ae38b806b7b4df9d591ce
MinexAutomation <minex.automation@gmail.com>
2019-10-17 16:17:42 -0700

    Added default method choice as extension method.

---
+++ source/R5T.Costobocia.Base/Code/Extensions/IOrganizationStringlyTypedPathOperatorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.Ostrogothia;
+
+
+namespace R5T.Costobocia
+{
+    public static class IOrganizationStringlyTypedPathOperatorExtensions
+    {
+        /// <summary>
+        /// Default method uses <see cref="IOrganizationStringlyTypedPathOperator.GetOrganizationDirectoryPathFromBaseDirectoryPath(string, IOrganization)"/>.
+        /// </summary>
+        public static string GetOrganizationDirectoryPath(this IOrganizationStringlyTypedPathOperator organizationStringlyTypedPathOperator, string baseDirectoryPath, IOrganization organization)
+        {
+            var output = organizationStringlyTypedPathOperator.GetOrganizationDirectoryPathFromBaseDirectoryPath(baseDirectoryPath, organization);
+            return output;
+        }
+    }
+}
